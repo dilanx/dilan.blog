0b744a178ef59497f4b634e1b621904165752a74
Dilan <dilan@dilanxd.com>
2023-04-22 02:19:12 -0500

    update blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,48 +106,7 @@ const config = {
             title: 'Blog',
             items: categories,
           },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Instagram',
-                href: 'https://www.instagram.com/dilan4k/',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/dilanx',
-              },
-              {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/in/dilann/',
-              },
-            ],
-          },
-          {
-            title: 'Resources',
-            items: [
-              {
-                label: 'System Status',
-                href: 'https://dilan.statuspage.io/',
-              },
-              {
-                label: 'Sponsor',
-                href: 'https://github.com/sponsors/dilanx',
-              },
-              {
-                label: 'Privacy',
-                href: 'https://www.dilanxd.com/privacy',
-              },
-              {
-                label: 'Attributions',
-                href: 'https://www.dilanxd.com/attributions',
-              },
-              {
-                label: 'Contact Me',
-                href: 'https://www.dilanxd.com/contact',
-              },
-            ],
-          },
+          ...require('@dilanx/themes/docusaurus-content').footer,
         ],
       },
       prism: {
